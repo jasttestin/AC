7654d3881fec53a2db66b539c84197fbded5e022
jasttestin <56484276+jasttestin@users.noreply.github.com>
2020-05-08 12:18:26 +0200

    updated mesage

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ client.on('message', async (message) => {
   if (message.channel.id == "707702167552786442") {
       message.channel.fetchMessages({ limit: 50 })
     .then(collected => {
+      const messages = collected.filter(msg => msg.author.id === client.user.id && msg.embeds.length === 1 ? msg.embeds[0].description === config.rules : false);
         message.channel.bulkDelete(messages).catch(console.error);
         message.channel.send({
           embed: {
@@ -49,9 +50,7 @@ client.on('message', async (message) => {
             color: 11344153,
           },
         });
-      }).catch(console.error);
-
-    console.log('deleted messages');
+    }).catch(console.error);
 
     return;
   }
